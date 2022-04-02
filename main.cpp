5491a1dcf5a5dfbcbcddf4352ecff2d5819297b5
Leaflet213 <msmk.swimmer@gmail.com>
2022-04-02 20:25:32 +0300

    finish homework

--- main.cpp
+++ main.cpp
@@ -8,11 +8,20 @@
 using std::cout;
 
 int main(int argc, char **argv) {
-
   // Пример. Создание пустого динамического массива.
-  assignment::DynamicArray array;
-
-  cout << "Write your code here...\n";
-
-  return 0;
+//    assignment::DynamicArray array(-1);
+    assignment::DynamicArray array1(4);
+    cout << array1.capacity() << "\n";
+  int size = 5;
+  int *massive = new int[size]{1, 2, 3, 4, 5};
+  size--;  // так удаляем элемент
+  int index = 2;
+  for (int i = index; i < size; i++) {
+    massive[i] = massive[i + 1];
+  }
+  int *data_ = new int[size];
+  std::copy(&massive[0], &massive[size], data_);
+  for (int i = 0; i < size; ++i) {
+    cout << data_[i] << "\n";
+  }
 }
